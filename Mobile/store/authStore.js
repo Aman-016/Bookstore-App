e1c7bd41033ba7b834f588ea24031924205f021c
Aman-016 <amanmehta.am2004@gmail.com>
2026-04-22 21:45:09 +0530

    updateauth store

--- Mobile/store/authStore.js
+++ Mobile/store/authStore.js
@@ -11,7 +11,7 @@ export const useAuthStore = create((set) => ({
   register: async (username, email, password) => {
     set({ isLoading: true });
     try {
-      const response = await fetch(`${API_URL}/auth/register`, {
+      const response = await fetch("https://bookstore-app-bdiz.onrender.com/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
